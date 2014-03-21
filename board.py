d5663f1d14d70b8d287e8784692627679b59fb4a
Randy Winsinger <randy.winsinger@las.ks.gov>
2014-03-20 20:52:05 -0500

    Added method to determine if cell empty and corresponding test case

--- board.py
+++ board.py
@@ -5,22 +5,39 @@
 class GameBoard:
     board = []              # Array to hold the board cells values 
     rowcols = 3             # Set the # of rows and cols (square)
-    markers = {0: " "}      # Dictionary for cell value to marker 
+    markers = {             # Dictionary for cell value to marker 
+        0: " ",
+        1: "O",
+        4: "X"
+        }
    
     """ Build empty board when object is created """
     def __init__(self):
         self.reset()
 
-    """ Give visual representation of board with players moves """
+    """
+      Checks to see if cell is empty
+      @returns boolean - true if value is 0, false if not 0
+    """
+    def is_cell_empty(self, loc):
+        return (self.board[loc] == 0)
+
+    """ 
+      Give visual representation of board with players moves 
+      @returns string - string representation of board's current state
+    """
     def __str__(self):
         h_char = '-'
         v_char = '|'
 
+        # Print top numbers
         display = " "
         for x in range(self.rowcols):
             display += "   " + str(x + 1)
         display += "\n"
 
+        # Loop through rows and columns, building board and showing
+        # cell values (if any)
         idx = 0
         for row in range(self.rowcols):
             display += "  " + h_char*13 + "\n"
@@ -39,5 +56,3 @@ class GameBoard:
     def reset(self):
         self.board = [0 for i in xrange(self.rowcols * self.rowcols)]
 
-board = GameBoard()
-print board
--- test_game.py
+++ test_game.py
@@ -7,6 +7,10 @@ class TestGameBoard(unittest.TestCase):
         game_board = GameBoard()
         self.assertIsInstance(game_board,GameBoard)
 
+    def test_is_empty(self):
+        game_board = GameBoard()
+        self.assertTrue(game_board.is_cell_empty(1))
+
 class TestPlayer(unittest.TestCase):
     def test_player_creation(self):
         player = Player("Randy")
